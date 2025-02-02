991dd17d2ed34a31e215f5940e122350708b324b
kch12 <gaemkyu4@gmail.com>
2025-02-02 17:25:19 +0900

    feat: 구분선 추가

---
+++ src/components/editor/customComponent/CustomLine.ts
@@ -0,0 +1,57 @@
+import { Node } from '@tiptap/core';
+
+const CustomLine = Node.create({
+  name: 'divider',
+
+  group: 'block',
+  atom: true,
+
+  addAttributes() {
+    return {
+      alignment: { default: 'mr-auto ml-0' },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'div.se-section.se-section-horizontalLine.se-l-default.se-section-align-center',
+        getAttrs: (element) => {
+          const alignment = element.classList.contains(
+            'se-section-align-center',
+          )
+            ? 'mx-auto'
+            : element.classList.contains('se-section-align-right')
+              ? 'ml-auto mr-0'
+              : 'mr-auto ml-0';
+
+          return { alignment };
+        },
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    const { alignment } = HTMLAttributes;
+    return [
+      'div',
+      {
+        class: `w-[220px] mt-[30px] relative w-full pt-[30px] pb-[29px] ${alignment}`,
+      },
+      [
+        'div',
+        { class: 'se-module se-module-horizontalLine __se-unit' },
+        ['span', { class: 'absolute inset-0' }],
+        [
+          'hr',
+          {
+            class:
+              'block border-0 m-0 p-0 block !m-0 mx-auto border-0 h-px bg-[#ddd]',
+          },
+        ],
+      ],
+    ];
+  },
+});
+
+export default CustomLine;
